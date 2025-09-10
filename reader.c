a25267894e617680ab1872295782297a7aa43ebe
PedazoDeCrack <129404650+PedazoDeCrack@users.noreply.github.com>
2025-09-09 21:01:24 -0600

    primero

---
+++ reader.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 1024
+
+void stream_char_test(){
+    FILE *fout, *fin;
+    char line[LEN +1];
+    int c;
+
+    fout  =fopen("test_file_char.txt", "wa");
+    printf("File descriptor of test_file_char.txt is %d\n", fileno(fout));
+    
+    fin =fdopen(0, "r");
+    if(fout == (FILE *)NULL)
+    exit(-1);
+
+    do{
+        c= fgetc(fin);
+        fputc(c, fout);
+    }while(c!= EOF);
+
+    fclose(fout);s
+}
